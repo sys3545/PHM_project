34b2aa8115e69c3578721edea81f9a06b60a3cce
Shim <sys3545@naver.com>
2021-04-06 17:55:43 +0900

    Create phm_pyhton_code.py

---
+++ UI/phm_pyhton_code.py
@@ -0,0 +1,48 @@
+from serial import Serial
+import tkinter
+from matplotlib import pyplot as plt
+from matplotlib import animation
+import random
+import time
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+PORT = 'COM5' # 포트 번호
+BaudRate = 9600 # 전송 속도
+ARD = Serial(PORT,BaudRate) # 아두이노와 연결되는 통신 시리얼 객체
+
+################
+ig = plt.figure(figsize = (11.4, 3))     #figure(도표) 생성
+
+ax = plt.subplot(211, xlim=(0, 100), ylim=(0, 50))
+
+max_points = 100
+
+line, = ax.plot(np.arange(max_points), 
+                np.ones(max_points, dtype=np.float)*np.nan, lw=1, c='blue',ms=1)
+
+def init():
+    return line
+
+def animate(i):
+    LINE = ARD.readline()
+    y = LINE[0]
+    y=int(y)
+
+    old_y = line.get_ydata()
+    new_y = np.r_[old_y[1:], y]
+    line.set_ydata(new_y)
+    print(LINE)
+    return line
+
+
+root = Tk.Tk() #추가
+root.geometry("1080x540+450+350")
+#label = Tk.Label(root,text="라벨").grid(column=0, row=0)#추가
+canvas = FigureCanvasTkAgg(fig, master=root) #
+canvas.get_tk_widget().grid(column=0,row=1) #
+
+anim = animation.FuncAnimation(fig, animate  , init_func= init ,frames=200, interval=50, blit=False)
+
+Tk.mainloop()
