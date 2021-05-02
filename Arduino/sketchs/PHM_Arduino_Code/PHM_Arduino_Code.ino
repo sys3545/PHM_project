97c1e3515c86d976e4cd05c93e89bda1ba7ffb8a
Shim <61923882+sys3545@users.noreply.github.com>
2021-05-02 17:24:05 +0900

    Update PHM_Arduino_Code.ino

--- Arduino/sketchs/PHM_Arduino_Code/PHM_Arduino_Code.ino
+++ Arduino/sketchs/PHM_Arduino_Code/PHM_Arduino_Code.ino
@@ -1,5 +1,6 @@
 #include <SoftwareSerial.h>
 #include <DHT.h>
+
 #define DHTPIN A1
 #define DHTTYPE DHT11
 
@@ -7,29 +8,29 @@ DHT dht(DHTPIN, DHTTYPE);
 SoftwareSerial BTSerial(8, 9); // 시리얼 통신 객체
 String message=""; // 메세지를 담는 임시 문자열
 char out[30]=""; // 최종 보낼 메시지
+int angle=0;
 
 void setup() {
   // put your setup code here, to run once:
   BTSerial.begin(9600); // 전송 속도 설정
   Serial.begin(9600); // 전송 속도 설정
+  pinMode(A0,INPUT); // 전류센서 인풋 핀
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-<<<<<<< HEAD
   int h = dht.readHumidity();
   int t = dht.readTemperature();
-
+  int v = analogRead(A0)-103;
   BTSerial.print('M');
   BTSerial.print(h);
   BTSerial.print(',');
   BTSerial.print(t);
+  BTSerial.print(',');
+  BTSerial.print(v);
+  BTSerial.print(',');
+  BTSerial.print(100);
   BTSerial.println();
-  delay(100);
-=======
-  current=digitalRead(SIG);
   
-  BTSerial.print(current);
-  BTSerial.println();
->>>>>>> 5319b1e59d6350d22585b8705c2169deba5867cf
+  delay(100);
 }
