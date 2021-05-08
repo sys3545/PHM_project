840fcf69d88c50494f3abfa7e038971f8cc7a02b
Shim <61923882+sys3545@users.noreply.github.com>
2021-05-08 20:53:23 +0900

    Update phm_pyhton_code.py

--- UI/phm_pyhton_code.py
+++ UI/phm_pyhton_code.py
@@ -17,6 +17,7 @@ BaudRate = 9600 # 전송 속도
 ARD = Serial(PORT,BaudRate) # 아두이노와 연결되는 통신 시리얼 객체
 l = []
 y=[0,0,0,0,0]
+count=0 # csv 인덱스
 
 fig = plt.figure(figsize = (8,6))
 gs  = GridSpec(nrows=4,ncols=2)
@@ -26,13 +27,13 @@ ax = fig.add_subplot(gs[0,0], xlim=(0, 100), ylim=(0, 40)) # [0,0]에 그림
 max_points = 100
 line, = ax.plot(np.arange(max_points), np.ones(max_points, dtype=np.float)*np.nan, lw=1, c='blue',ms=1)
 ax.get_xaxis().set_visible(False) # x축 제거
-ax.set_title("value_1",fontsize =9)
+ax.set_title("Sounds",fontsize =9)
 
 ax_2 = fig.add_subplot(gs[1,0], xlim=(0, 100), ylim=(0, 40))
 max_points_2 = 100
 line_2, = ax_2.plot(np.arange(max_points_2), np.ones(max_points_2, dtype=np.float)*np.nan, lw=1, c='green',ms=1)
 ax_2.get_xaxis().set_visible(False) # x축 제거
-ax_2.set_title("value_2",fontsize =9)
+ax_2.set_title("Vibration",fontsize =9)
 
 
 ax_3 = fig.add_subplot(gs[2,0], xlim=(0, 100), ylim=(-20, 50))
@@ -42,11 +43,11 @@ ax_3.get_xaxis().set_visible(False) # x축 제거
 ax_3.set_title("Electric Current",fontsize =9)
 
 
-ax_4 = fig.add_subplot(gs[3,0], xlim=(0, 100), ylim=(0, 110))
+ax_4 = fig.add_subplot(gs[3,0], xlim=(0, 100), ylim=(480, 520))
 max_points_4 = 100
 line_4, = ax_4.plot(np.arange(max_points_4), np.ones(max_points_4, dtype=np.float)*np.nan, lw=1, c='black',ms=1)
 ax_4.get_xaxis().set_visible(False) # x축 제거
-ax_4.set_title("value_4",fontsize =9)
+ax_4.set_title("Voltage",fontsize =9)
 
 
 #################################
@@ -81,9 +82,13 @@ def animate(i):
         ARD.flushInput()
 
     print(LINE)
+    global count;
     l.append(LINE) # 리스트 추가
-    # df = pd.DataFrame(l, columns=['Humidity', 'Temperature','100','100'])
-    # df.to_csv('sensor_data.csv', index=False, encoding='cp949')  # csv 생성
+    df = pd.DataFrame(l, columns=['Sounds', 'vibration','Electric Current','Voltage'])
+    #df = pd.DataFrame.from_dict([{'Sounds':LINE[0],'vibration':LINE[1],'Electric Current':LINE[2],'Voltage':LINE[3]}])
+    #df = pd.DataFrame({'Sounds':LINE[0],'vibration':LINE[1],'Electric Current':LINE[2],'Voltage':LINE[3]}, index=[count])
+    count+=1
+    df.to_csv('sensor_data.csv', index=True, encoding='cp949')  # csv 생성
     return line,
 
 def animate_2(i):   
