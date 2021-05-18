22a5f3d6dfa0b88d98802446ca30e5e6c031d2dd
Shim <61923882+sys3545@users.noreply.github.com>
2021-05-18 14:59:24 +0900

    Update phm_pyhton_code.py

--- UI/phm_pyhton_code.py
+++ UI/phm_pyhton_code.py
@@ -22,9 +22,12 @@ y=[0,0,0,0,0]
 count=0 # csv 인덱스
 ts = 200 
 
-fig = plt.figure(figsize = (8,6))
-gs  = GridSpec(nrows=4,ncols=2)
-
+fig = plt.figure(figsize = (4,4))
+plt.suptitle("Data - Non prediction")
+fig2 = plt.figure(figsize = (8,4))
+plt.suptitle("Data - Prediction")
+gs  = GridSpec(nrows=2,ncols=1)
+gs2 = GridSpec(nrows=2,ncols=2)
 
 ax = fig.add_subplot(gs[0,0], xlim=(0, 100), ylim=(0, 80)) # [0,0]에 그림
 max_points = 100
@@ -32,36 +35,36 @@ line, = ax.plot(np.arange(max_points), np.ones(max_points, dtype=np.float)*np.na
 ax.get_xaxis().set_visible(False) # x축 제거
 ax.set_title("Sounds",fontsize =9)
 
-ax_2 = fig.add_subplot(gs[0,1], xlim=(0, 100), ylim=(0, 40))
+ax_2 = fig.add_subplot(gs[1,0], xlim=(0, 100), ylim=(0, 40))
 max_points_2 = 100
 line_2, = ax_2.plot(np.arange(max_points_2), np.ones(max_points_2, dtype=np.float)*np.nan, lw=1, c='green',ms=1)
 ax_2.get_xaxis().set_visible(False) # x축 제거
 ax_2.set_title("Temperature",fontsize =9)
 
 
-ax_3 = fig.add_subplot(gs[2,0], xlim=(0, 100), ylim=(-20, 50))
+ax_3 = fig2.add_subplot(gs2[0,0], xlim=(0, 100), ylim=(-20, 50))
 max_points_3 = 100
 line_3, = ax_3.plot(np.arange(max_points_3), np.ones(max_points_3, dtype=np.float)*np.nan, lw=1, c='red',ms=1)
 ax_3.get_xaxis().set_visible(False) # x축 제거
 ax_3.set_title("Electric Current",fontsize =9)
 
 
-ax_4 = fig.add_subplot(gs[3,0], xlim=(0, 100), ylim=(480, 520))
+ax_4 = fig2.add_subplot(gs2[1,0], xlim=(0, 100), ylim=(480, 520))
 max_points_4 = 100
 line_4, = ax_4.plot(np.arange(max_points_4), np.ones(max_points_4, dtype=np.float)*np.nan, lw=1, c='black',ms=1)
 ax_4.get_xaxis().set_visible(False) # x축 제거
 ax_4.set_title("Voltage",fontsize =9)
 
 
-ax_7 = fig.add_subplot(gs[2,1], xlim=(0, 100), ylim=(-20, 50))
+ax_7 = fig2.add_subplot(gs2[0,1], xlim=(0, 100), ylim=(-20, 50))
 max_points_3 = 100
 ax_7.get_xaxis().set_visible(False) # x축 제거
 ax_7.set_title("Predicted Electric Current",fontsize =9)
 
-ax_8 = fig.add_subplot(gs[3,1], xlim=(0, 100), ylim=(-20, 50))
+ax_8 = fig2.add_subplot(gs2[1,1], xlim=(0, 100), ylim=(-20, 50))
 max_points_3 = 100
 ax_8.get_xaxis().set_visible(False) # x축 제거
-ax_8.set_title("Predicted Electric Current",fontsize =9)
+ax_8.set_title("Predicted Voltage",fontsize =9)
 
 #################################
 def init():
@@ -101,17 +104,17 @@ def animate(i):
     df_csv.to_csv('sensor_data.csv', index=True, encoding='cp949')  # csv 생성
     count+=1
 
-    if count==200:
+    if count==201:
         col_list = ['Electric Current']
         dataframe = pd.read_csv('sensor_data.csv', usecols=col_list)
         df = np.array(dataframe).flatten()
         df = pd.Series(df)
-        N=df.size
-        mean = df.mean()
+        N=df.size # 데이터 사이즈 
+        mean = df.mean() 
         std = np.std(df)
         df = df.map(lambda x : (x - mean) / std) # 정규화
       
-        model = load_model('phm_model.h5')
+        model = load_model('phm_model.h5') # 모델 로드
 
         df_new=df[-ts:]
         df_new = np.asarray([np.array([df.values[i+j] for j in range(ts)])
@@ -119,7 +122,7 @@ def animate(i):
 
         global ax_7
         ax_7.remove()
-        ax_7 = fig.add_subplot(gs[2,1], xlim=(0, 100), ylim=(-20, 50))
+        ax_7 = fig2.add_subplot(gs2[0,1], xlim=(0, 100), ylim=(-20, 50))  
         max_points_3 = 100
         ax_7.get_xaxis().set_visible(False) # x축 제거
         ax_7.set_title("Predicted Electric Current",fontsize =9)
@@ -130,10 +133,9 @@ def animate(i):
                 df_new[-1][j]=df_new[-1][j+1]
             df_new[-1][-1]=pred
         
-        print(df_new[-1])
+
         X=np.array(range(0,ts))
         df_new[-1] = (df_new[-1]*std)+mean
-        print(df_new[-1])
         ax_7.plot(X,df_new[-1],'r-')
         count=0
 
@@ -160,16 +162,22 @@ def animate_4(i):
 
     return line_4 
 
-root = Tk.Tk() #추가
-root.geometry("1080x700+350+100")
 
+## UI ##
+root = Tk.Tk() #추가 
+root.title("PHM")
+root.geometry("1200x520+120+50")
+root.resizable(False,False)
+root.configure(bg='white')
 canvas = FigureCanvasTkAgg(fig, master=root)
-canvas.get_tk_widget().grid(column=0,row=1) 
+canvas.get_tk_widget().grid(column=0,row=0) 
 
+canvas2 = FigureCanvasTkAgg(fig2, master=root)
+canvas2.get_tk_widget().grid(column=1,row=0) 
 
 anim = animation.FuncAnimation(fig, animate  , init_func= init ,frames=200, interval=50, blit=False)
 anim_2 = animation.FuncAnimation(fig, animate_2 ,init_func= init_2 ,frames=200, interval=50, blit=False)
-anim_3 = animation.FuncAnimation(fig, animate_3 ,init_func= init_3 ,frames=200, interval=50, blit=False)
-anim_4 = animation.FuncAnimation(fig, animate_4 ,init_func= init_4 ,frames=200, interval=50, blit=False)
+anim_3 = animation.FuncAnimation(fig2, animate_3 ,init_func= init_3 ,frames=200, interval=50, blit=False)
+anim_4 = animation.FuncAnimation(fig2, animate_4 ,init_func= init_4 ,frames=200, interval=50, blit=False)
 
 Tk.mainloop()
